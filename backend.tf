1a59511f6965820bb9c49319778ebc558cbe89f8
Manoharan <manoharan.s@gmail.com>
2025-08-03 21:08:42 +0800

    changed tfstate path

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "s3-tfstate-golang-api"
-    key            = "env-name/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "ap-southeast-1"
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
